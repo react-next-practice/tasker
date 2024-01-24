ded90217143a101ffa15cb5ccea172773992de11
Jahid Hasan <jahid0676@gmail.com>
2024-01-24 20:10:03 +0600

    Edit Task

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import TaskBoard from "./components/task/TaskBoard";
+export default function App() {
+    return (
+        <>
+            <Header />
+            <div className="flex flex-col justify-center items-center">
+                <Hero />
+                <TaskBoard />
+            </div>
+            <Footer />
+        </>
+    );
+}
---
+++ src/components/task/TaskBoard.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import AddTaskModal from "./AddTaskModal";
+import SearchTask from "./SearchTask";
+import TaskActions from "./TaskActions";
+import TaskList from "./TaskList";
+
+export default function TaskBoard() {
+    const defaultTask = {
+        id: crypto.randomUUID(),
+        title: "Learn React Native",
+        description:
+            "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
+        tags: ["web", "react", "js"],
+        priority: "High",
+        isFavorite: false,
+    };
+
+    // Define a state
+    const [tasks, setTasks] = useState([defaultTask]);
+    const [showTaskModal, setShowTaskModal] = useState(false);
+    const [taskToEdit, setTaskToEdit] = useState(null);
+
+    // handle Add Task Modal
+    function handleAddTaskModal( newTask, isAdd ) {
+        if( isAdd ){
+            setTasks([...tasks, newTask]);
+        }else{
+            const updatedTasks = tasks.map((task) => {
+                if (task.id === newTask.id) {
+                    return newTask;
+                }
+                return task;
+            });
+            
+            setTasks(updatedTasks);
+        }
+
+        setShowTaskModal(false);
+    }
+
+    // handle Edit Task
+    function handleEditTask(task) {
+        setTaskToEdit(task);
+        setShowTaskModal(true);
+    }
+
+    // Handle close
+    function handleOnCloseClick() {
+        setShowTaskModal(false);
+        setTaskToEdit(null);
+    }
+    
+    return (
+        <>
+            <section className="mb-20" id="tasks">
+                {showTaskModal && <AddTaskModal onCreateTask={handleAddTaskModal} taskToEdit={taskToEdit} onCloseClick={handleOnCloseClick} />}
+
+                <div className="container">
+                    <div className="p-2 flex justify-end">
+                        <SearchTask />
+                    </div>
+
+                    <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+                        <TaskActions onAddTask={() => setShowTaskModal(true)} />
+                        <TaskList tasks={tasks} onEditTask={handleEditTask} />
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
